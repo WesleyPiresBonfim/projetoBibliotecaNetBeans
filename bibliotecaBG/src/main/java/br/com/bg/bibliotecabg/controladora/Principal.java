2cc1d9bf39ad9fb7c1e4174a34d46d0fd8a3b33f
WesleyPiresBonfim <wesley.piresbonfim@gmail.com>
2018-09-20 20:38:15 -0300

    Criação dos Pacotes e Preparação da Classe

---
+++ bibliotecaBG/src/main/java/br/com/bg/bibliotecabg/controladora/Principal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.bg.bibliotecaBG.controladora;
+
+import br.com.bg.bibliotecaBG.tela.TelaMenu;
+
+
+/**
+ *
+ * @author Wesley
+ * @since 20/09/2018
+ */
+public class Principal {
+    public static void main(String[] args) {
+        TelaMenu telaMenu = new TelaMenu();
+        telaMenu.setVisible(true);
+    }
+}
